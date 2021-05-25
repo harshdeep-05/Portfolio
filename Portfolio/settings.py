398f8c6c1dc9de505689a8f5e5d4f6500b657085
harshdeep-05 <harshdeepsahu5@gmail.com>
2021-05-25 23:47:18 +0530

    enter

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm7r1eke50x!o1kumbj=dzi273u%yt86lcg5wesm9gq&vqpo)1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
