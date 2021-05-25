4a001e737cf1dbef78885735336a259662d18f1e
harshdeep-05 <harshdeepsahu5@gmail.com>
2021-05-25 22:16:39 +0530

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Skills :
+    type = str
+    name = str
+    level = int
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-
+from .models import Skills
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html') 
+
+    skill1=Skills()
+    skill1.type = 'Languages'
+    skill1.name = 'C++'
+    skill1.level = 4
+
+    skill2=Skills()
+    skill2.type = 'Frontend'
+    skill2.name = ['Html','CSS']
+    skill2.level = 3
+
+    skill3=Skills()
+    skill3.type = 'Backend'
+    skill3.name = 'Django'
+    skill3.level = 3
+
+    skill4=Skills()
+    skill4.type = 'Creative'
+    skill4.name = 'Guitar'
+    skill4.level = 5
+
+    skills= [skill1,skill2,skill3,skill4]
+    return render(request, 'blog/home.html', {'skills': skills}) 
