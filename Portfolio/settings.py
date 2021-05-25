67f855723cf42028e7b9e798d58cadabb8e52c03
harshdeep-05 <harshdeepsahu5@gmail.com>
2021-05-26 00:17:01 +0530

    commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, '/harshdeep16.pythonanywhere.com/Potrfolio/static')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'Portfolio/static')
 ]
