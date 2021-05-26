ebb95b190ee949df1ac5e8013bb2f0a80aa4394c
harshdeep-05 <harshdeepsahu5@gmail.com>
2021-05-26 18:12:25 +0530

    commit

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "120px";
-    document.getElementById("main").style.marginLeft = "150px";
-  }
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }  
