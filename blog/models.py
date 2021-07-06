df1add492e610adbf4e1d20f239d05d39593110b
harshdeep-05 <harshdeep@gmail.com>
2021-07-06 12:27:27 +0530

    changes in home

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class Skills :
-    type = str
-    name = str
-    level = int
+
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('blog/',views.Blog, name='blog')
 ]
